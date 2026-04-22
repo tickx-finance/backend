8a8713a0c32267626b102c65e5168a58fbad18e8
aa-sniperman <nhatducmo@gmail.com>
2026-04-23 00:00:00 +0700

    fix: order query swagger

--- src/modules/order/dto/get-orders.dto.ts
+++ src/modules/order/dto/get-orders.dto.ts
@@ -1,18 +1,22 @@
 import { IsEnum, IsNumber, IsOptional, Min } from 'class-validator';
 import { Type } from 'class-transformer';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { OrderStatus } from '../types';
 
 export class GetOrdersDto {
+    @ApiPropertyOptional({ enum: OrderStatus, description: 'Filter by order status' })
     @IsOptional()
     @IsEnum(OrderStatus)
     status?: OrderStatus;
 
+    @ApiPropertyOptional({ type: Number, default: 20, description: 'Number of orders to return (min: 1)' })
     @IsOptional()
     @Type(() => Number)
     @IsNumber()
     @Min(1)
     limit?: number = 20;
 
+    @ApiPropertyOptional({ type: Number, default: 0, description: 'Number of orders to skip (min: 0)' })
     @IsOptional()
     @Type(() => Number)
     @IsNumber()
